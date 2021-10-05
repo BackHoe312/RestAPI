f0d28b6c9e962c0db2243731b49ca36c984e2454
backhoe312 <idb3131@naver.com>
2021-10-05 11:16:42 +0900

    #2.6 Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './movies/app.controller';
 
 @Module({ // 데코레이터. 데코레이터는 꾸며주는 함수나 클래스랑 부텅있어야 함
-  imports: [],
-  controllers: [MoviesController], // URL을 가져오고 함수를 실행 (=express.js의 controller/router)
-  providers: [MoviesService], // 실질적으로 사용하는 함수를 정의하는 부분
+  imports: [MoviesModule],
+  controllers: [AppController], // URL을 가져오고 함수를 실행 (=express.js의 controller/router)
+  providers: [], // 실질적으로 사용하는 함수를 정의하는 부분
 })
 export class AppModule {}
